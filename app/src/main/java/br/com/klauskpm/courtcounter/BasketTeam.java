a61c7c5cb21b52c748fccd1b2ff3faafdaec4c7c
Klaus Kazlauskas <klausmachado@gmail.com>
2016-05-26 12:07:40 -0300

    Funções de pontos para os times de basket

--- app/src/main/java/br/com/klauskpm/courtcounter/BasketTeam.java
+++ app/src/main/java/br/com/klauskpm/courtcounter/BasketTeam.java
@@ -1,8 +1,38 @@
 package br.com.klauskpm.courtcounter;
 
+import android.view.View;
+import android.widget.TextView;
+
 /**
  * Created by Kazlauskas on 26/05/2016.
  */
 public class BasketTeam {
-    
+
+    TextView teamScore;
+
+    public BasketTeam(View v) {
+        teamScore = (TextView) v;
+    }
+
+    public void threePointsThrow() {
+        addPoints(3);
+    }
+
+    public void twoPointsThrow() {
+        addPoints(2);
+    }
+
+    public void freeThrow() {
+        addPoints(1);
+    }
+
+    private void addPoints(Integer points) {
+        Integer previousPoints = Integer.parseInt((String) teamScore.getText());
+
+        teamScore.setText("" + (previousPoints + points));
+    }
+
+    public void reset() {
+        teamScore.setText("" + 0);
+    }
 }
--- app/src/main/java/br/com/klauskpm/courtcounter/MainActivity.java
+++ app/src/main/java/br/com/klauskpm/courtcounter/MainActivity.java
@@ -2,12 +2,69 @@ package br.com.klauskpm.courtcounter;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import org.w3c.dom.Text;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView teamAScore;
+    private TextView teamBScore;
+
+    private BasketTeam BasketTeamA;
+    private BasketTeam BasketTeamB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        teamAScore = (TextView) findViewById(R.id.team_a__score__text_view);
+        teamBScore = (TextView) findViewById(R.id.team_b__score__text_view);
+
+        BasketTeamA = new BasketTeam(teamAScore);
+        BasketTeamB = new BasketTeam(teamBScore);
+    }
+
+    public void threePointsThrow(View v) {
+        switch (v.getId()) {
+            case R.id.team_a__three_points__button:
+                BasketTeamA.threePointsThrow();
+                break;
+
+            case R.id.team_b__three_points__button:
+                BasketTeamB.threePointsThrow();
+                break;
+        }
+    }
+
+    public void twoPointsThrow(View v) {
+        switch (v.getId()) {
+            case R.id.team_a__two_points__button:
+                BasketTeamA.twoPointsThrow();
+                break;
+
+            case R.id.team_b__two_points__button:
+                BasketTeamB.twoPointsThrow();
+                break;
+        }
+    }
+
+    public void freeThrow(View v) {
+        switch (v.getId()) {
+            case R.id.team_a__free_throw__button:
+                BasketTeamA.freeThrow();
+                break;
+
+            case R.id.team_b__free_throw__button:
+                BasketTeamB.freeThrow();
+                break;
+        }
+    }
+
+    public void reset(View v) {
+        BasketTeamA.reset();
+        BasketTeamB.reset();
     }
 }
